541d278faffba846422b19d8eb7d83c2532138e3
Cem Asma <cem.asma@getir.com>
2021-09-13 23:29:29 +0300

    add flag option for file input

--- main.go
+++ main.go
@@ -1,14 +1,19 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"time"
 )
 
 func main() {
 
+	file := flag.String("file", "647.json", "specify the mapping file you want to solve\n--file 647.json")
+
+	flag.Parse()
+
 	startDate := time.Now()
-	game := NewGame("647.json")
+	game := NewGame(*file)
 
 	result := game.solve()
 	endDate := time.Now()
